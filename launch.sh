5dc0121ba602d8b7352febfe24e2039e53b7ca13
Robert Steger <rsteger1144@gmail.com>
2021-09-11 20:30:37 -0400

    correcting launch.sh to upgrade db, not migrate

--- launch.sh
+++ launch.sh
@@ -2,8 +2,6 @@
 #windows (command prompt) commands
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.DevelopmentConfig"
-flask db init
-flask db migrate
 flask db upgrade
 #flask run --host=0.0.0.0 --port=5000
 #flask run
